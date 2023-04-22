7fd85b62a866fdd07fc4fd62214351e737c5419c
Bhanu Prakash Singh <bhanu.singh@gmail.com>
2023-04-21 22:31:39 -0700

    basic linux cell commnds

---
+++ bhanu/compilation_stages/test.c
@@ -0,0 +1,11 @@
+#include "test.h"
+
+int main()
+	{
+#if YES
+		printf("MAX:%d\n",MAX);
+#else
+		printf("sum=%d\n",SUB(a,b));
+#endif
+		return 0;
+	}
---
+++ bhanu/compilation_stages/test.h
@@ -0,0 +1,4 @@
+#define MAX 10
+#define SUB(a,b) (a+b)
+#define YES 1
+int a,b;
