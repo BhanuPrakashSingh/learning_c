38ea3cf9afe96f6fbde716b30a03fca8e378110d
Adity Singh <6390adityasingh@gmail.com>
2023-04-29 08:35:09 -0400

    size of data type

---
+++ bit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("size of (int): %u\n",sizeof(int));
+	printf("size of (char): %u\n",sizeof(char));
+	printf("size of (float): %u\n",sizeof(float));
+	printf("size of (long): %u\n",sizeof(long));
+	printf("size of (double): %u\n",sizeof(double));
+	printf("size of (short): %u\n",sizeof(short));
+	printf("size of (long double): %u\n",sizeof(long double));
+	
+	return 0;
+}
+
