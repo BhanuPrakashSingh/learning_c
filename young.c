61455a14a5769956e163969967dfccd2a57f6459
MansiSingh2 <117916600+MansiSingh2@users.noreply.github.com>
2023-05-12 20:31:15 +0530

    Update young.c

--- young.c
+++ young.c
@@ -4,9 +4,9 @@ int main() {
 	int Ram, Shyam, Ajay;
 	printf("Enter the age of Ram, Shyam and Ajay : \n");
 	scanf("%d%d%d",&Ram,&Shyam,&Ajay);
-	if(Ram > Shyam && Ram > Ajay)
+	if(Ram < Shyam && Ram < Ajay)
 	    printf("Ram is youngest.\n");
-	else if(Shyam > Ram && Shyam > Ajay)
+	else if(Shyam < Ram && Shyam < Ajay)
 	    printf("Shyam  is youngest.\n");
 	else 
 	    printf("Ajay is youngest.\n");
