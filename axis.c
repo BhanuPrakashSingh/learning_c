323a5e0742069fba49522a01353ae0d252e9e0c6
MansiSingh2 <117916600+MansiSingh2@users.noreply.github.com>
2023-05-13 21:15:46 +0530

    Update axis.c

--- axis.c
+++ axis.c
@@ -4,13 +4,6 @@ int main()
     int x, y;
     printf("Enter points(x, y) : \n");
     scanf("%d%d",&x,&y);
-    if(x == 0 && y != 0)
-        printf("Points (%d, %d) lies on y-axis\n",x,y);
-    else if(x != 0 && y == 0)
-        printf("Points (%d, %d) lies on x-axis\n",x,y);
-    else if(x == 0 && y == 0)
-        printf("Points (%d, %d) lies on the origin\n",x,y);
-    else 
-        printf("Points (%d, %d) neither lies on x-aixs or y-aixs\n",x,y);
+    (x == 0) ? ((y!=0) ? printf("(%d,%d)on y-axis\n") : printf("(%d,%d)on origin\n")) : ((y==0) ? printf("(%d,%d)on x-axis\n") : printf("(%d,%d)nighter on x&y-axis\n"));
     return 0;
 }
