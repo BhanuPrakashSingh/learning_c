5e341312a98452f737c088cf0c5deb199340c191
MansiSingh2 <117916600+MansiSingh2@users.noreply.github.com>
2023-05-13 21:07:22 +0530

    Update coorder1.c

--- coorder1.c
+++ coorder1.c
@@ -2,12 +2,14 @@
 #include<math.h>
 int main()
 {
-    int x, y, r, d;
-    printf("Enter coordinates (x, y) of center of a circle:\n");
+    int x, y,cx, cy r, d;
+    printf("Enter points (cx, cy) of center of a circle:\n");
+    scanf("%d%d",&cx,&cy);
+    printf("Enter points (x, y) to check the points:\n");
     scanf("%d%d",&x,&y);
     printf("Enter redius of a circle: \n");
     scanf("%d",&r);
-    d = sqrt(pow(x, 2) + pow(y, 2));
+    d = sqrt(pow((x - cx), 2) + pow((y - cy), 2));
     if(d < r)
         printf("Points (%d, %d) is inside the circle\n",x,y);
     else if(d > r)
