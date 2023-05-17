67a99caa4993fed7849c4289d97c23285b1e3472
Aditya Singh <111952139+Aditya-pix@users.noreply.github.com>
2023-05-17 10:19:24 +0530

    Check number is Armstrong or not

---
+++ armstrong12.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+	int n,count,temp,rem,sum=0,temp1;
+	scanf("%d",&n);
+	temp=temp1=n;
+	while(n!=0)
+	{
+		count++;
+		n=n/10;
+	}
+	while(temp!=0)
+	{
+		rem=temp%10;
+		sum=sum+pow(rem,count);
+		temp=temp/10;
+	}
+	if(temp1==sum)
+	printf("Armstrong");
+	else
+	printf("Not armstrong");
+
+}
