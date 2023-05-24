5663fe0e887926c03dac6d50dca3afe42461a26c
Adity Singh <6390adityasingh@gmail.com>
2023-05-24 14:12:33 -0400

    Loop

---
+++ loop/Count.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int posi=0,zero=0,neg=0,num;
+	printf("Enter the number till you want.\nEnter 999 for Quit\n");
+	while(1)
+	{
+		scanf("%d",&num);
+		if(num==999)
+                        break;
+		if(num>0)
+
+			posi++;
+		if(num<0)
+
+			neg++;
+		if(num==0)
+			zero++;
+//		num=0;
+	}
+	printf("Positive: %d\tNegative: %d\tZero: %d\n",posi,neg,zero);
+	return 0;
+}
--- loop/ascii.c
+++ loop/ascii.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	unsigned int i=0;
+	int i=0;
 	while(i<=255)
 	{
 		printf("ASCII value of character%d : %c\n ",i,i );
