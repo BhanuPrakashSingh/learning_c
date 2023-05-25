d6f35af0c7b18db5d1bae5d3ef82e7d3c2a3a214
Adity Singh <6390adityasingh@gmail.com>
2023-05-25 10:53:28 -0400

    Integer to Octal

---
+++ Loop1/oct.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+	int n,sum=0,rem,i=1;
+	printf("Enter positive integer number\n");
+	scanf("%d",&n);
+	 while(n>0)
+	 {
+		 rem=n%8;
+                sum=sum+rem*i;
+		i*=10;
+                n/=8;
+
+	 }
+	 /*
+	rem=0;
+	n=sum;
+	sum=0;
+	while(n>0)
+	{
+	     rem=n%10;
+	     sum=sum*10+rem;
+	     n/=10;
+	}*/
+	printf("%d ",sum);
+}
